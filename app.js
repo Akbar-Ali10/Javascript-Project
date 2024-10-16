c5aba270896e631865f6ecb58c519b7cea4f88dd
Ali <baltimuhammad38@gmail.com>
2024-10-16 16:54:52 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+
+
+function search(){
+    let textTosearch = document.getElementById("textTosearch").value;
+    let para = document.getElementById("para");
+    textTosearch = textTosearch.replace(/[.*+?^${}()]|[\]\\]/g,"\\$&");
+
+    let pattern = new RegExp(`${textTosearch}`,"gi");
+
+    para.innerHTML = para.textContent.replace(pattern, match => `<mark>${match}</mark>`)
+    
+}
+
